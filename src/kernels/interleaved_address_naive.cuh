0e6ee42212dda4af8852da90be4a76a94c8e4980
Yajas <shivhitayajusuman@gmail.com>
2026-06-21 09:52:59 +0530

    implemented naive interleaved address reduction

---
+++ src/kernels/interleaved_address_naive.cuh
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+template<size_t NUM_THREADS>
+__global__ void batched_interleaved_address_naive(
+  float* __restrict__ Y,
+  float* const __restrict__ X
+) {
+  __shared__ float shared_data[NUM_THREADS];
+  X += blockDim.x * blockIdx.x;
+
+  shared_data[threadIdx.x] = X[threadIdx.x];
+  __syncthreads();
+  
+  for (int stride = 1; stride < NUM_THREADS; stride *= 2) {
+    if(threadIdx.x % 2*stride == 0) {
+      shared_data[threadIdx.x] += shared_data[threadIdx.x + stride];
+    }
+    __syncthreads();
+  }
+  
+  if (threadIdx.x == 0) {
+    Y[blockIdx.x] = shared_data[0];
+  }
+}
